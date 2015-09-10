12d153d170a905b4748df60b068022df5b865f32
Rajiv Bose <rbose85@users.noreply.github.com>
2015-09-10 14:29:50 +0100

    rename project alias

--- devel.zsh
+++ devel.zsh
@@ -4,7 +4,8 @@
 # ALL THE SOFTWARE TINGS
 
 # what's the plurarl of 'alias'?
-alias jim2x="cd ~/Code/commercial/mnetics/IM2X-Client/IM2X-Shell && nvm use"
-alias jiwazat="cd ~/Code/commercial/iwazat/iwazat && nvm use"
-alias jiiwazat="cd ~/Code/commercial/iwazat/iwazat-ionic-app && nvm use"
+alias jiwazat="cd ~/Code/commercial/iwazat/projects-legacy/iwazat && nvm use"
+alias jiiwazat="cd ~/Code/commercial/iwazat/projects-legacy/iwazat-ionic-app && nvm use"
+alias jtt3iwazat="cd ~/Code/commercial/iwazat/projects-t3/hybrid-app && nvm use"
+
 alias jles="cd ~/Code/commercial/daredev/lesEssence && nvm use"
