0f19e91f8ba5b227d4a97edb615f6b4d03720b78
Rajiv Bose <rbose85@users.noreply.github.com>
2016-11-18 12:35:44 +0000

    add helpful ssh command for arcadia

--- devel.zsh
+++ devel.zsh
@@ -9,3 +9,7 @@
 # what's the plurarl of 'alias'?
 alias jles="cd ~/Code/commercial/daredev/lesEssence && nvml"
 alias jfull="cd ~/Code/commercial/arcadiagroup/full-monty && nvml"
+
+
+# ssh-shotcuts
+alias ssh-arcadia-digital="ssh -i ~/.ssh/pems/arcadia-digital-bs ec2-user@ec2-52-57-78-247.eu-central-1.compute.amazonaws.com"
